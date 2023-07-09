ac20aa50d03def30a9f80ad5a5db294949756ec0
ms_udee <u.uduakdavid@gmail.com>
2023-07-09 19:12:31 +0100

    task 3 (PSEUDOCODE/FLOWCHART)

---
+++ js/pseudo.js
@@ -0,0 +1,10 @@
+let clubberAge = prompt("Enter Your Age");
+let vipAge = 35;
+if (clubberAge < vipAge) {
+  console.log("Not Up to VIP Age");
+} else if (clubberAge >= vipAge) {
+  console.log("Welcome to the VIP section");
+} else {
+  console.log("invalid: Enter a number");
+}
+ur;
