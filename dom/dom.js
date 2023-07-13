ecae79146f62327f4442bd4af2374ee51f2eae65
ms_udee <u.uduakdavid@gmail.com>
2023-07-13 06:44:43 +0100

    practising

---
+++ dom/dom.js
@@ -0,0 +1,40 @@
+// PRACTISE PRACTISE PRACTISE
+
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", don);
+// function don() {
+//   alert(btn.textContent);
+// }
+// setInterval(don, 1000);
+// console.log(btn.classList.add("goo"));
+// console.log(btn);
+
+// btn.addEventListener("click", press);
+// function press() {
+//   document.body.classList.add("darkmod");
+// }
+// const image = document.querySelector("img");
+// image.alt = "i love my calculator";
+// image.setAttribute("alt", "i need a better calculator");
+// console.log(image.getAttribute("alt"));
+// // console.log(image.hasAttribute("alt"));
+// console.log(image.dataset);
+// console.log(image.dataset.productnane);
+// console.log(image.dataset.description);
+// image.addEventListener("click", touch);
+// function touch() {
+//   image.classList.add("boo");
+//   alert(image.dataset.description);
+// }
+// const heading = document.createElement("h3");
+// heading.textContent = "hello hello";
+// console.dir(heading);
+// const imgs = document.createElement("img");
+// imgs.setAttribute("src", "/images/pseudo.jpg");
+// console.dir(imgs);
+// heading.appendChild(imgs);
+// console.log(heading);
+// const main = document.createElement("main");
+// const div = document.createElement("div");
+// main.appendChild(div);
+// console.log(main);
